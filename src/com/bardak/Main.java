695e63d323697d7004e57e6c3c4d06b192c6c152
Valeriy Ezhov <nerucoder@gmail.com>
2019-08-31 17:22:46 +0300

    add CommandConsoleService

--- src/com/bardak/Main.java
+++ src/com/bardak/Main.java
@@ -1,5 +1,6 @@
 package com.bardak;
 
+import com.bardak.service.CommandConsoleService;
 import com.bardak.service.TaskService;
 import com.bardak.storage.InMemoryTaskStorage;
 import com.bardak.storage.TaskStorage;
@@ -8,11 +9,10 @@ public class Main {
     public static void main(String args[]) {
         TaskStorage storage = new InMemoryTaskStorage();
         TaskService service = new TaskService(storage);
-        service.addTask("one", "two");
-        service.addTask("two", "own");
-        service.showAll();
-        service.markAsDone("one");
-        System.out.println("***************");
-        service.showAll();
+        CommandConsoleService commandConsoleService = new CommandConsoleService(service);
+        while (true) {
+            commandConsoleService.getMenu();
+            commandConsoleService.handleCommand(commandConsoleService.getUserInput());
+        }
     }
 }
