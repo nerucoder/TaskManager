24c320c4efe0e3ff20ce844061331b0341855494
Valeriy Ezhov <nerucoder@gmail.com>
2019-08-31 16:41:58 +0300

    clean code

--- src/com/bardak/storage/InMemoryTaskStorage.java
+++ src/com/bardak/storage/InMemoryTaskStorage.java
@@ -7,26 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class InMemoryTaskStorage implements TaskStorage {
-    /*private ArrayList<Task> list = new ArrayList<Task>();
-    @Override
-    public void saveTask(Task task) {
-        list.add(task);
-    }
-
-    @Override
-    public ArrayList<Task> getAll() {
-        return list;
-    }
-
-    @Override
-    public Task getByName(String name) {
-        for (Task task : list) {
-            if (task.getName().equals(name)) {
-                return task;
-            }
-        }
-        return null;
-    }*/
 
     private Map<String, Task> map = new HashMap<>();
 
--- src/com/bardak/storage/TaskStorage.java
+++ src/com/bardak/storage/TaskStorage.java
@@ -5,7 +5,7 @@ import com.bardak.domain.Task;
 import java.util.ArrayList;
 
 public interface TaskStorage {
-    public void saveTask(Task task);
-    public ArrayList<Task> getAll();
-    public Task getByName(String name);
+    void saveTask(Task task);
+    ArrayList<Task> getAll();
+    Task getByName(String name);
 }
